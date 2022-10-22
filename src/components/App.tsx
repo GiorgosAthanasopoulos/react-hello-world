147303e6eb9868a9b7d0f997b785df13eda55ae1
giorgosathanasopoulos <spzdoom@gmail.com>
2022-10-22 20:57:31 +0300

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../stylesheets/App.css";
+
+export default function App(): JSX.Element {
+    return (
+        <>
+            <h1 id={"title"}>Hello, World!</h1>
+        </>
+    );
+};
